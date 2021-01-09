0780e8a8d371a3a857542721ea01af6e69ce18db
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-09 16:03:34 +0100

    Fixed bug with AppLoading

--- App.js
+++ App.js
@@ -1,38 +1,38 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import * as Font from 'expo-font'
-import * as AppLoading from 'expo-app-loading'
+import AppLoading from 'expo-app-loading'
 import MealsNavigator from "./navigation/MealsNavigator";
 
 const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  })
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    })
 }
 
 export default function App() {
 
-  const [fontsLoaded, setFontsLoaded] = useState(false)
+    const [fontLoaded, setFontLoaded] = useState(false);
 
-  if (!fontsLoaded) {
-    return <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setFontsLoaded(true)}
-        onError={(err) => { console.log(err) }}
-    />
-  }
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setFontLoaded(true)}
+                onError={console.warn}
+            />
+        );
+    }
 
-  return (
-    <MealsNavigator/>
-  );
+    return <MealsNavigator/>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
