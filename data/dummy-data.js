94a50c5a795b58e886fe99fe5e28bd35d29977d1
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-11 15:39:26 +0100

    Added model of Categories and some data

---
+++ data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from "../models/category";
+
+export const CATEGORIES = [
+    new Category('c1', 'Włoska', '#f5428d'),
+    new Category('c2', 'Polska', '#f54242'),
+    new Category('c3', 'Amerykańska', '#f5a442'),
+    new Category('c4', 'Niemiecka', '#f5d142'),
+    new Category('c5', 'Tajska', '#368dff'),
+    new Category('c6', 'Chińska', '#41d95d'),
+    new Category('c7', 'Gruzińska', '#9eecff'),
+    new Category('c8', 'Indonezyjska', '#b9ffb0'),
+    new Category('c9', 'Francuska', '#ffc7ff'),
+    new Category('c10', 'Grecka', '#47fced')
+];
