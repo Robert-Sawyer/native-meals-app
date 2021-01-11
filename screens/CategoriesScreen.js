d2e7bce3f88979ca1d771ebf0f4d0b9bed7d6198
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-11 15:41:01 +0100

    Added FlatList with Categories + some comments

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import {View, Text, Button, StyleSheet} from 'react-native'
+import {View, Text, Button, StyleSheet, FlatList} from 'react-native'
+
+import {CATEGORIES} from "../data/dummy-data";
+
+const renderGridItem = (itemData) => {
+    return (
+        <View>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    )
+}
 
 const CategoriesScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>Some normal text!</Text>
+            <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
             {/*Tworzę button w celu przejścia na inna stronę w nawigacji, konkretnie z przepisami z jednej kuchni
             wewnątrz obiektu props jest wiele różnych wbudowanych metod m.in. navigation, która pozwala na wykorzystanie
             funkcji navigate któa przyjmuje obiekt a w nim we właściwości routeName wpisuję jako string nazwę
@@ -12,6 +22,15 @@ const CategoriesScreen = props => {
             w MealsNavigator
             UPDATE: alternatywnie można zamiast obiektu i routeName zastosować tylko to, co poniżej, czyli
             po prostu navigate('string z nazwą')*/}
+            {/*Są inne alternatywne sposoby na wyrendereowanie strony poprzez nawigację,
+            np. poprzez .push('Przepisy') - wtedy można przechodzić w kółko do tego samego ekranu, tzn nawet
+             jeśli przechodzę z ekranu Przepisy na ekran Przepisy to i tak nastąpi przejście, tylko, że do tego samego
+             ekranu (zastosowanie przy przechodzeniu z folderu do folderu ale z inną zawartością w apkach typu DropBox)
+            poprzez .goBack(), albo pop() - bez zawartości. Te metody usuwają ekran ze stosu i wracają na poprzedni ekran.
+            poprzez .popToTop() - wraca do pierwszej strony (pierwszej na początku na stosie),
+            poprzez .replace('Przepisy') - zastępuje ekran na stosie; usuwa ze stosu pierwotnie pierwszy i ustawia nowy
+            ekran na dole stosu - nie można wrócić do pierwotnie pierwszej strony (ma zastosowanie np. po logowaniu,
+            gdy user się loguje i nie chcę, żeby mógł wrócić do ekranu logowania bez wcześniejszego wylogowania)*/}
             <Button title='Przejdź dalej' onPress={() => {
                 props.navigation.navigate('Przepisy')
             }} />
