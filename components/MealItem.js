524a1d26576c7692b088d9c2c98a47ce3a65ec53
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-14 18:11:22 +0100

    Created Meal Item component

---
+++ components/MealItem.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    Platform,
+    ImageBackground
+} from 'react-native'
+
+const MealItem = props => {
+    let TouchableComp = TouchableOpacity
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableComp = TouchableNativeFeedback
+    }
+
+    return (
+        <View style={styles.mealItem}>
+            <TouchableComp onPress={props.onSelectMeal}>
+                <View>
+                    <View style={{...styles.mealRow, ...styles.mealHeader}}>
+                        <ImageBackground source={{uri: props.image}} style={styles.bgImage}>
+                            <View style={styles.titleContainer}>
+                                <Text style={styles.title} numberOfLines={1}>{props.title}</Text>
+                            </View>
+                        </ImageBackground>
+                    </View>
+                    <View style={{...styles.mealRow, ...styles.mealDetails}}>
+                        <Text>{props.duration}m</Text>
+                        <Text>{props.complexity.toUpperCase()}</Text>
+                        <Text>{props.affordability.toUpperCase()}</Text>
+                    </View>
+                </View>
+            </TouchableComp>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    mealItem: {
+        height: 200,
+        width: '100%',
+        backgroundColor: '#e7e6e6',
+        marginVertical: 5,
+        borderRadius: 10,
+        overflow: 'hidden',
+    },
+    mealRow: {
+        flexDirection: 'row',
+    },
+    bgImage: {
+        width: '100%',
+        height: '100%',
+        // justifyContent: 'flex-end'
+    },
+    mealHeader: {
+        height: '85%',
+    },
+    titleContainer: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        paddingVertical: 5,
+        paddingHorizontal: 12,
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 20,
+        color: '#fff',
+        textAlign: 'center',
+    },
+    mealDetails: {
+        height: '15%',
+        paddingHorizontal: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+})
+
+export default MealItem
