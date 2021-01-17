b19d466567ea3ea4c10d4877a31e35bab65cee02
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-17 21:33:48 +0100

    Added Meallist comp and implemented it in CatMealsScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import {View, FlatList, StyleSheet} from 'react-native'
 import {CATEGORIES, MEALS} from "../data/data";
-import MealItem from "../components/MealItem";
+import MealList from "../components/MealList";
 
 
 const CategoryMealsScreen = props => {
@@ -13,33 +12,8 @@ const CategoryMealsScreen = props => {
     //wybrana przez usera
     const displayedMeals = MEALS.filter(meal => meal.categoryId.indexOf(categoryId) >= 0)
 
-    const renderMealItem = itemData => {
-        return (
-            <MealItem
-                title={itemData.item.title}
-                duration={itemData.item.duration}
-                complexity={itemData.item.complexity}
-                affordability={itemData.item.affordability}
-                image={itemData.item.imageUrl}
-                onSelectMeal={() => {
-                    props.navigation.navigate('MealDetailsScreen',
-                        {
-                            mealId: itemData.item.id
-                        })
-                }}
-            />
-        )
-    }
-
     return (
-        <View style={styles.screen}>
-            <FlatList
-                data={displayedMeals}
-                keyExtractor={(item, index) => item.id}
-                renderItem={renderMealItem}
-                style={{width: '100%'}}
-            />
-         </View>
+        <MealList listData={displayedMeals} navigation={props.navigation}/>
     )
 }
 
@@ -50,14 +24,5 @@ CategoryMealsScreen.navigationOptions = navigationData => {
         headerTitle: `Kuchnia ${selectedCategory.title}`,
     }
 }
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 15,
-    },
-})
 
 export default CategoryMealsScreen
