b262778f5905e50fc48c9af53de0075fa97ffb12
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-19 13:35:12 +0100

    Added content to filters screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, { useState } from 'react'
+import {View, Text, StyleSheet, Switch, Platform} from 'react-native'
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
-import FavoritesScreen from "./FavoritesScreen";
+import Colors from "../constants/Colors";
+
+const FiltersSwitch = props => {
+    return (
+        <View style={styles.filtersContainer}>
+            <Text>{props.label}</Text>
+            <Switch
+                // trackColor={{true: Colors.tabBarColor}}
+                // thumbColor={Platform.OS === 'android' ? Colors.tabBarColor : ''}
+                value={props.state}
+                onValueChange={props.onChange}
+            />
+        </View>
+    )
+}
 
 const FiltersScreen = props => {
+
+    const [isGlutenFree, setIsGlutenFree] = useState(false)
+    const [isLactoseFree, setIsLactoseFree] = useState(false)
+    const [isVegan, setIsVegan] = useState(false)
+    const [isVegetarian, setIsVegetarian] = useState(false)
+
     return (
         <View style={styles.screen}>
-            <Text>Some text</Text>
+            <Text style={styles.title}>Dostępne filtry</Text>
+            <FiltersSwitch label='Bezglutenowe' state={isGlutenFree} onChange={newValue => setIsGlutenFree(newValue)}/>
+            <FiltersSwitch label='Bez laktozy' state={isLactoseFree} onChange={newValue => setIsLactoseFree(newValue)}/>
+            <FiltersSwitch label='Wegańskie' state={isVegan} onChange={newValue => setIsVegan(newValue)}/>
+            <FiltersSwitch label='Wegetariańskie' state={isVegetarian} onChange={newValue => setIsVegetarian(newValue)}/>
         </View>
     )
 }
@@ -15,8 +39,20 @@ const FiltersScreen = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center',
+    },
+    filtersContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 10,
+        width: '60%',
     },
 })
 
