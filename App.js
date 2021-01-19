968cc798846be41a791ad3dcc7abb78ebf38d6ad
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-19 15:25:24 +0100

    Installed redux and added store to the app

--- App.js
+++ App.js
@@ -4,9 +4,18 @@ import * as Font from 'expo-font'
 import AppLoading from 'expo-app-loading'
 import MealsNavigator from "./navigation/MealsNavigator";
 import {enableScreens} from 'react-native-screens'
+import {createStore, combineReducers} from 'redux'
+import mealsReducer from "./store/reducers/meals";
+import {Provider} from 'react-redux'
 
 //dodaję enableScreens aby zwiększyć wydajność - nie wpływa to na wygląd an funkcjonowanie apki
 enableScreens()
+
+const rootReducer = combineReducers({
+    meals: mealsReducer,
+})
+
+const store = createStore(rootReducer)
 
 const fetchFonts = () => {
     return Font.loadAsync({
@@ -29,14 +38,9 @@ export default function App() {
         );
     }
 
-    return <MealsNavigator/>;
+    return (
+        <Provider store={store}>
+            <MealsNavigator/>
+        </Provider>
+    )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
