992aeb63e931cea139d83ae837f0e0152f97e2f6
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-19 12:19:45 +0100

    Added main content in Meal details screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,19 +1,36 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, ScrollView, Image} from 'react-native'
 import {HeaderButtons, Item} from 'react-navigation-header-buttons'
+import DefaultText from "../components/DefaultText";
 import {MEALS} from "../data/data";
 import CustomHeaderButton from "../components/HeaderButton";
+
+const ListItem = props => {
+    return (
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
+        </View>
+    )
+}
 
 const MealDetailsScreen = props => {
 
     const mealId = props.navigation.getParam('mealId')
-
     const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-        </View>
+        <ScrollView>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}min</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Składniki</Text>
+            {selectedMeal.ingredients.map(ingredient => <ListItem key={ingredient}>{ingredient}</ListItem>)}
+            <Text style={styles.title}>Kroki</Text>
+            {selectedMeal.steps.map(step => <ListItem key={step}>{step}</ListItem>)}
+        </ScrollView>
     )
 }
 
@@ -23,8 +40,8 @@ MealDetailsScreen.navigationOptions = navigationData => {
     return {
         headerTitle: selectedMeal.title,
         headerRight: () =>
-        //HeaderButtons wymaga zdefiniowania komponentu, którym jest mój CHB, w Item za to definiuję nazwę ikony
-        //z pakietu w dokumentacji expo-vectors
+            //HeaderButtons wymaga zdefiniowania komponentu, którym jest mój CHB, w Item za to definiuję nazwę ikony
+            //z pakietu w dokumentacji expo-vectors
             <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
                 <Item title='Favorite' iconName='ios-star-outline' onPress={() => {
                     console.log('Wciskam buttona')
@@ -35,10 +52,27 @@ MealDetailsScreen.navigationOptions = navigationData => {
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    details: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 15,
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center',
+        marginVertical: 10,
+    },
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    listItem: {
+        marginVertical: 5,
+        marginHorizontal: 20,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc'
     },
 })
 
