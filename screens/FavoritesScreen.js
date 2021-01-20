1633e75e2d035b5c7b12f366f283a005485ff593
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-20 18:13:29 +0100

    Added text if it's no favorites on the list

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import {View, StyleSheet} from 'react-native'
 import {useSelector} from 'react-redux'
 import MealList from "../components/MealList";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 const FavoritesScreen = props => {
 
     //dobieram się tutaj do ulubionych przepisów i to je wrzucam do listy wyświetlanych przepisów
     const favMeals = useSelector(state => state.meals.favoriteMeals)
+
+    if (favMeals.length === 0 || !favMeals) {
+        return (
+            <View style={styles.text}>
+                <DefaultText>Nie masz żadnych ulubionych przepisów</DefaultText>
+            </View>
+        )
+    }
 
     return (
         <MealList listData={favMeals} navigation={props.navigation}/>
@@ -29,5 +39,13 @@ FavoritesScreen.navigationOptions = navData => {
             </HeaderButtons>
     }
 }
+
+const styles = StyleSheet.create({
+    text: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
 
 export default FavoritesScreen
